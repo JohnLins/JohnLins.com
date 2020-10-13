0fc118090d636305cd3a1e9f4cdc8c0be89e6914
John Lins <[johnpaullins@gmail.com]>
2020-10-12 20:14:31 -0700

    New Nav

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggle() {
+  var x = document.getElementById("mobile_nav_items");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
