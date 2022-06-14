2556f7610b5b54fdaacdc9edb0be2d687ca4a966
John Lins <johnpaullins@gmail.com>
2022-06-13 22:49:38 -0700

    auto

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,4 @@
-
+/*
 function des(name, id){
 $.ajax({
     type: "GET",
@@ -40,4 +40,79 @@ function stats(name, id){
   
   }
   
+
+*/
+  function listout(projects, id){
+
+
+
+    for(let i = 0; i < projects.length; i++){
+      let images = ""
+      $.ajax({
+        type: "GET",
+        url: "https://api.github.com/repos/johnlins/" + projects[i].name,
+    
+   
+        
+        dataType: "json",
+        success: function(result) {
+
+          
+            
+          for(let j = 0; j < projects[i].imgs.length; j++){
+            images = images + `<img src="`+ projects[i].imgs[j] +`" height="100"/>`
+          }
+
+          $("#"+id).append(`
+          
+          
+          <div class="project">
+          <h3>`+projects[i].title+`</h3>
+          <br/>
+          `+result.description+`
+          <br/>`+
+
+          images
+          
+          +`
+          <p class="sub">` + result.created_at.slice(0, 10) + ` to `+ result.updated_at.slice(0, 10) + ` &#183; <b>` + result.language + `</b> &#183; <a href='` + result.html_url + `' target='_blank'>REPO</a> 
+        `+ ((typeof projects[i].vid !== 'undefined') ? (`</b> &#183; <a href='` + projects[i].vid + `' target='_blank'>VID</a>`) : ``)  +`
+          </p><br/><span>
+          `+
+          ((typeof projects[i].award !== 'undefined') ? (`&#127942; `+projects[i].award) : ``)
+          +`
+          </span>
+          </div>`)
+
+        }
+    });
+    }
+
+
+  }
   
+
+  function listoutworkshops(workshops, id){
+
+    var insert = document.getElementById(id);
+
+
+
+    for(let i = 0; i < workshops.length; i++){
+      if(workshops[i].vid != ""){
+        video = `<div clas="sub"><a href="`+workshops[i].vid+`">VIDEO</a></div>`;
+      }
+
+      insert.innerHTML += `<div class="project">
+          <h3>`+workshops[i].title+`</h3>
+          <br/>
+          <iframe src="https://docs.google.com/presentation/d/e/` + workshops[i].code + `/embed?start=true&loop=true&delayms=1000" frameborder="0" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+          <br/>`
+          +video+`</div>`
+
+    }
+
+    
+
+    
+  }
